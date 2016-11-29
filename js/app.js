2c6c991ef1d0fd68e494a347bc2d05dd2cfccf2f
David Shin <davidshin444@gmail.com>
2016-11-28 16:45:29 -0800

    Copied from wd-ajax-hero

--- js/app.js
+++ js/app.js
@@ -1,60 +1,108 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  const movies = [];
+    var movies = [];
 
-  const renderMovies = function() {
-    $('#listings').empty();
+    var renderMovies = function() {
+        $('#listings').empty();
 
-    for (const movie of movies) {
-      const $col = $('<div class="col s6">');
-      const $card = $('<div class="card hoverable">');
-      const $content = $('<div class="card-content center">');
-      const $title = $('<h6 class="card-title truncate">');
+        for (var movie of movies) {
+            var $col = $('<div class="col s6">');
+            var $card = $('<div class="card hoverable">');
+            var $content = $('<div class="card-content center">');
+            var $title = $('<h6 class="card-title truncate">');
 
-      $title.attr({
-        'data-position': 'top',
-        'data-tooltip': movie.title
-      });
+            $title.attr({
+                'data-position': 'top',
+                'data-tooltip': movie.title
+            });
 
-      $title.tooltip({ delay: 50, });
-      $title.text(movie.title);
+            $title.tooltip({
+                delay: 50,
+            });
+            $title.text(movie.title);
 
-      const $poster = $('<img class="poster">');
+            var $poster = $('<img class="poster">');
 
-      $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
-      });
+            $poster.attr({
+                src: movie.poster,
+                alt: `${movie.poster} Poster`
+            });
 
-      $content.append($title, $poster);
-      $card.append($content);
+            $content.append($title, $poster);
+            $card.append($content);
 
-      const $action = $('<div class="card-action center">');
-      const $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
+            var $action = $('<div class="card-action center">');
+            var $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
 
-      $plot.attr('href', `#${movie.id}`);
-      $plot.text('Plot Synopsis');
+            $plot.attr('href', `#${movie.id}`);
+            $plot.text('Plot Synopsis');
 
-      $action.append($plot);
-      $card.append($action);
+            $action.append($plot);
+            $card.append($action);
 
-      const $modal = $(`<div id="${movie.id}" class="modal">`);
-      const $modalContent = $('<div class="modal-content">');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+            var $modal = $(`<div id="${movie.id}" class="modal">`);
+            var $modalContent = $('<div class="modal-content">');
+            var $modalHeader = $('<h4>').text(movie.title);
+            var $movieYear = $('<h6>').text(`Released in ${movie.year}`);
+            var $modalText = $('<p>').text(movie.plot);
 
-      $modalContent.append($modalHeader, $movieYear, $modalText);
-      $modal.append($modalContent);
+            $modalContent.append($modalHeader, $movieYear, $modalText);
+            $modal.append($modalContent);
 
-      $col.append($card, $modal);
+            $col.append($card, $modal);
 
-      $('#listings').append($col);
+            $('#listings').append($col);
 
-      $('.modal-trigger').leanModal();
+            $('.modal-trigger').leanModal();
+        }
+    };
+
+    // ADD YOUR CODE HERE
+
+    /// Get the input
+    var input = $('input');
+
+    /// Click handler for button to start Ajax request
+    $('button').click(function() {
+        /// Prevent default message from refreshing the browser and losing data
+        event.preventDefault();
+
+        /// Clear previous movies from array
+        movies = [];
+
+        /// Do Ajax request if form is filled
+        if ($(input).val() !== "") {
+            let userInput = $(input).val();
+            sendAjaxRequest(userInput);
+        }
+    });
+
+    function sendAjaxRequest(search) {
+        // Request object for AJAX
+        let requestObject = {
+            url: `https://www.omdbapi.com/?s=${search}&y=&plot=short&r=json`,
+            method: "GET",
+            success: handleSuccess
+        };
+        // Start the AJAX request
+        $.ajax(requestObject);
     }
-  };
 
-  // ADD YOUR CODE HERE
+    // Builds the movie list with movies in appropriate format
+    function handleSuccess(data) {
+        movies = data.Search.map(function(item){
+            return {
+                poster: item.Poster,
+                title: item.Title,
+                type: item.Type,
+                year: item.Year,
+                id: item.imdbID
+            }
+        })
+
+        renderMovies()
+    }
+
+
 })();
